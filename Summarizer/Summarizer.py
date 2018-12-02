5da34c1cf4c49a87641d2b6fef803e62253bb456
Surya Ram <rasurya@amazon.comm>
2018-12-02 15:13:18 -0800

    Added support for url-based doc source

--- Summarizer/Summarizer.py
+++ Summarizer/Summarizer.py
@@ -9,12 +9,26 @@ from string import punctuation
 from heapq import nlargest
 from collections import defaultdict
 
+import urllib.request
+
+import bs4 as bs
+import lxml
+
 
 def main():
 	args = parse_arguments()
 	
+	if(args.sourcetype == 'filepath'):
+		content = read_file(args.filepath)
+	elif(args.sourcetype == 'url'):
+		content = read_url(args.filepath)
+	else:
+		print("Error: Document location needs to be either a Filepath, or a URL. Other formats not currently available.")
+		exit()
 
-	content = read_file(args.filepath)
+
+	
+
 	content = sanitize_input(content)
 	
 	sent_tokens, word_tokens = tokenize_content(content)
@@ -23,11 +37,14 @@ def main():
 
 	print(summarize(sentence_rankings, sent_tokens, args.length))
 
+	exit()
+
 
 def parse_arguments():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('filepath', help="File Name of text doc to summarize.")
 	parser.add_argument('-l', '--length', default=4, help="Number of sentences to return")
+	parser.add_argument('-s', '--sourcetype', default='filepath', help="Type of source: file or url")
 	args = parser.parse_args()
 
 	return args
@@ -39,6 +56,19 @@ def read_file(fp):
 			return file.read()
 	except IOError as e:
 		print("Fatal Error, File at {} could not be located or is not readable.".format(fp))
+
+
+def read_url(url):
+	page = urllib.request.urlopen(url).read()
+
+	soup = bs.BeautifulSoup(page, 'lxml')
+
+	content = ""
+	for paragraph in soup.find_all('p'):
+		content += paragraph.text
+
+	return content
+
 
 
 def sanitize_input(data):
